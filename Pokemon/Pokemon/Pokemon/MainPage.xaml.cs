d6810dadcd86dd3c3bc411e13638184725b16301
aschi2403 <jakob@familie-aschauer.eu>
2019-12-12 06:51:30 +0100

    first commit

---
+++ Pokemon/Pokemon/Pokemon/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Pokemon
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly HttpClient client = new HttpClient();
+        public ObservableCollection<Pokemon> PokemonList { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.PokemonList = new ObservableCollection<Pokemon>
+            {
+                new Pokemon{Name = "This", Url = "https://robohash.org/test.png"}
+            };
+            BindingContext = this;
+        }
+
+        void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null; // de-select the row
+            Navigation.PushModalAsync(new Details());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var pokemonList = this.GetPokemonList().Result;
+            this.PokemonList = pokemonList;
+
+        }
+
+        private async Task<ObservableCollection<Pokemon>> GetPokemonList()
+        {
+            var response = await client.GetAsync("https://pokeapi.co/api/v2/pokemon");
+            var pokemonList = new ObservableCollection<Pokemon>();
+
+            if (response.IsSuccessStatusCode)
+            {
+                var stream = await response.Content.ReadAsStreamAsync();
+                pokemonList = await JsonSerializer.DeserializeAsync<ObservableCollection<Pokemon>>(stream);
+                
+            }
+
+            return pokemonList;
+        }
+    }
+}
---
+++ Pokemon/Pokemon/Pokemon/Pokemon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        
+    }
+
+    public class PokemonResponse
+    {
+        public Pokemon[] Results { get; set; }
+    }
+    public class Ability
+    {
+        public string Name { get; set; }
+    }
+
+    public class PokemonDetails
+    {
+        public string Name { get; set; }
+        public string Weight { get; set; }
+
+        public AbilitySlot[] Abilities { get; set; }
+        public Sprite Sprites { get; set; }
+    }
+
+    public class Sprite
+    {
+        public Uri Front_default { get; set; }
+
+        public Uri Back_default { get; set; }
+    }
+
+    public class AbilitySlot
+    {
+        public Ability Ability { get; set; }
+    }
+
+
+}
